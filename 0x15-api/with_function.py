f2dce2c888489cf1aa20f001891f11099448ddf1
Gogo-Eng <progressgogochinda@gamil.com>
2024-05-22 04:56:31 +0100

    api

--- 0x15-api/with_function.py
+++ 0x15-api/with_function.py
@@ -9,32 +9,26 @@ import requests
 import sys
 
 
-if __name__ == "__main__":
-    try:
-        employee_id = int(sys.argv[1])
-    except ValueError:
-        print("Please provide a valid integer as the employee ID.")
-        sys.exit(1)
-
-    if len(sys.argv) != 2:
-        print(f"Usage: python3 script.py {employee_id}")
+def TODO_list(employee_id):
 
     url = "https://jsonplaceholder.typicode.com/"
     user_response = requests.get(f"{url}users/{employee_id}")
 
     if user_response.status_code != 200:
         print("Unable to fetch user")
+        return
 
     user_data = user_response.json()
     employee_name = user_data.get('name')
     if not employee_name:
         print("Employee name not found")
+        return
 
     todo_response = requests.get(f"{url}todos?userId={employee_id}")
 
     if todo_response.status_code != 200:
         print("Unable to fetch TODO data")
-
+        return
     todo_data = todo_response.json()
     completed_tasks = []
     for task in todo_data:
@@ -47,3 +41,16 @@ if __name__ == "__main__":
     print(f"Employee {employee_name} is done with tasks({nct}/{tnt}):")
     for tasks in completed_tasks:
         print(f"\t {tasks}")
+
+
+if __name__ == "__main__":
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Please provide a valid integer as the employee ID.")
+        sys.exit(1)
+
+    if len(sys.argv) != 2:
+        print(f"Usage: python3 script.py {employee_id}")
+
+    TODO_list(employee_id)
