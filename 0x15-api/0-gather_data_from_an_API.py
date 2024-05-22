04c009072d2e4f255c82e1a6d577222b322b40ff
Gogo-Eng <progressgogochinda@gamil.com>
2024-05-22 05:02:50 +0100

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,44 +1,28 @@
 #!/usr/bin/python3
-"""
-a Python script that, using a REST API, for a given
-employee ID, returns information about his/her T
-ODO list progress.
-"""
+'''Write a Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.'''
 
-import requests
-import sys
+from requests import get
+from sys import argv
 
 
-if __name__ == "__main__":
-    employee_id = int(sys.argv[1])
-    if len(sys.argv) != 2:
-        print(f"Usage: python3 script.py {employee_id}")
+if __name__ == '__main__':
+    user_id = argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+    response = get(url)
+    name = response.json().get('name')
 
-    url = "https://jsonplaceholder.typicode.com/"
-    user_response = requests.get(f"{url}users/{employee_id}")
+    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)
+    response = get(url)
+    tasks = response.json()
+    done = 0
+    done_tasks = []
+    for task in tasks:
+        if task.get('completed'):
+            done_tasks.append(task)
+            done += 1
 
-    if user_response.status_code != 200:
-        print("Unable to fetch user")
-
-    user_data = user_response.json()
-    employee_name = user_data.get('name')
-    if not employee_name:
-        print("Employee name not found")
-
-    todo_response = requests.get(f"{url}todos?userId={employee_id}")
-
-    if todo_response.status_code != 200:
-        print("Unable to fetch TODO data")
-
-    todo_data = todo_response.json()
-    completed_tasks = []
-    for task in todo_data:
-        if task.get('completed') is True:
-            completed_tasks.append(task.get('title'))
-
-    nct = len(completed_tasks)
-    tnt = len(todo_data)
-
-    print(f"Employee {employee_name} is done with tasks({nct}/{tnt}):")
-    for tasks in completed_tasks:
-        print(f"\t {tasks}")
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, done, len(tasks)))
+    for task in done_tasks:
+        print("\t {}".format(task.get('title')))
