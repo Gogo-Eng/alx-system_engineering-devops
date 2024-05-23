7bbb6c34458c4627368c48c641dd98412dad0b91
Gogo-Eng <progressgogochinda@gamil.com>
2024-05-23 16:42:45 +0100

    json

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,47 +1,32 @@
 #!/usr/bin/python3
-"""
-a Python script that, using a REST API, for all given
-employee ID, returns information about all employees
-TODO list progress and exports the data in the json format.
-"""
+''' Using what you did in the task #0, extend your Python script to export data
+in the JSON format.'''
 
-import json
-import requests
+from json import dump
+from requests import get
 
 
-def TODO_list():
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/users/'
+    response = get(url)
+    users = response.json()
 
-    url = "https://jsonplaceholder.typicode.com/"
-    user_response = requests.get(f"{url}users")
-
-    if user_response.status_code != 200:
-        print("Unable to fetch user")
-        return
-
-    user_data = user_response.json()
-
-    data = {}
-
-    for user in user_data:
-        user_id = user["id"]
-        user_name = user["username"]
-        todo_response = requests.get(f"{url}users/{user_id}/todos")
-        todo_data = todo_response.json()
-        data[user_id] = []
-
-        for task in todo_data:
+    dictionary = {}
+    for user in users:
+        user_id = user.get('id')
+        username = user.get('username')
+        url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+        url = url + '/todos/'
+        response = get(url)
+        tasks = response.json()
+        dictionary[user_id] = []
+        for task in tasks:
             task_list = {
-                        'username': user_name,
-                        'task': task["title"],
-                        'completed': task["completed"],
-                        }
-            data[user_id].append(task_list)
+                "username": username,
+                "task": task.get('title'),
+                "completed": task.get('completed'),
+                }
+            dictionary[user_id].append(task_list)
 
-    filename = "todo_all_employees.json"
-    with open(filename, mode='w', encoding='utf-8') as file:
-        json.dump(data, file)
-    print(f"Data have been exported into {filename}")
-
-
-if __name__ == "__main__":
-    TODO_list()
+    with open('todo_all_employees.json', 'w') as file:
+        dump(dictionary, file)
