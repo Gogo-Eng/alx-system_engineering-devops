a0f4637eb2ed01ac7f278865257cb8a7a658c285
Gogo-Eng <progressgogochinda@gamil.com>
2024-05-23 17:20:42 +0100

    json

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -12,7 +12,7 @@ import requests
 if __name__ == "__main__":
 
     url = "https://jsonplaceholder.typicode.com/"
-    user_response = requests.get(f"{url}users")
+    user_response = requests.get(f"{url}users/")
 
     if user_response.status_code != 200:
         print("Unable to fetch user")
@@ -22,9 +22,9 @@ if __name__ == "__main__":
     data = {}
     for user in user_data:
         user_id = user["id"]
-        todo_response = requests.get(f"{url}users/{user_id}/todos")
-        todo_data = todo_response.json()
         user_name = user["username"]
+        todo_response = requests.get(f"{url}users/{user_id}/todos/")
+        todo_data = todo_response.json()
         data[user_id] = []
 
         for task in todo_data:
